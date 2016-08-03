71b1ed5c9a298f3d9b9e36674570377856847597
EliphasNUIT <bichon.jekfer@gmail.com>
2016-08-03 10:38:43 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  if (myName !== '') {
+	localStorage.setItem('name', myName);
+	myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }	else {
+	setUserName();  
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
