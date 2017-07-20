cdef0e484e5d959cf90f490fe7d93010de7c4030
Jekfer Bichon <jekfer.bichon@3ds.com>
2017-07-20 17:36:29 +0200

    problème des accents fixés)

--- HTMLBuilder/src/boss.cpp
+++ HTMLBuilder/src/boss.cpp
@@ -17,22 +17,22 @@ void Boss::generateHTMLs(){
     res << "<!DOCTYPE html>";
     res <<"<html>";
     res << "<head>";
-    res <<    "<meta charset=\"latin\">";
-    res <<    "<title>[ODS] " + this->name + " Logs</title>";
-    res <<    "<meta name=\"author\" content=\"EliphasNUIT\">";
-    res <<    "<link href=\"../styles/style.css\" rel=\"stylesheet\" type=\"text/css\">";
-    res <<  "</head>";
-    res <<  "<body>";
-    res <<    "<h1>" + this->name + " [ODS]</h1>";
-    res <<    this->imagePath;
-    res <<    "<p> <a href=\"../index.html\">Index</a> </p>";
-    res <<    "<p>Les try</p> ";
-    res <<        "<ul> ";
-    res <<    this->getTries();
-    res <<        "</ul>";
+    res << "    <meta charset=\"latin\">";
+    res << "    <title>[ODS] " + this->name + " Logs</title>";
+    res << "    <meta name=\"author\" content=\"EliphasNUIT\">";
+    res << "    <link href=\"../styles/style.css\" rel=\"stylesheet\" type=\"text/css\">";
+    res << "</head>";
+    res << "<body>";
+    res << "    <h1>" + this->name + " [ODS]</h1>";
+    res << this->imagePath;
+    res << "    <p> <a href=\"../index.html\">Index</a> </p>";
+    res << "    <p>Les try</p> ";
+    res << "        <ul> ";
+    res << this->getTries();
+    res << "        </ul>";
 
-    res <<    "<p>Tous les logs ont été créés par <a href=\"https://www.deltaconnected.com/arcdps/\">ArcDPS</a> et parsés par <a href=\"https://raidheroes.tk/\">Raid Heroes</a>.</p>";
-    res <<  "</body>";
+    res << QString::fromLatin1("    <p>Tous les logs ont été créés par <a href=\"https://www.deltaconnected.com/arcdps/\">ArcDPS</a> et parsés par <a href=\"https://raidheroes.tk/\">Raid Heroes</a>.</p>");
+    res << "</body>";
     res <<"</html>";
 
     QStringListIterator it(res);
