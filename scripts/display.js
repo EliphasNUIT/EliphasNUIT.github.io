3dc6f63cfa9bf9bbd313f2f675fd34c68c479ed5
Jekfer Bichon <bichon.jekfer@gmail.com>
2017-08-23 22:28:00 +0200

    archivages log

--- scripts/display.js
+++ scripts/display.js
@@ -1,15 +1,11 @@
 function display(frameID) {
     var iframe = $(frameID);
     var status = iframe.attr("status");
-    var timeout = 0;
     if (status === "closed") {
         iframe.attr("src", iframe.data("src"));
         iframe.load(function () {
             iframe.width("1200");
-            iframe.height(iframe.contents().height());
-            timeout = setTimeout(function() {
-                iframe.height(iframe.contents().height());
-            },500);
+            iframe.height(1.25*iframe.contents().height());
             iframe.attr("status", "open");
         });
     } else {
@@ -17,9 +13,6 @@ function display(frameID) {
         iframe.load(function () {
             iframe.width("1");
             iframe.height("1");
-            if (timeout) {
-                clearTimeout(timeout);
-            }
             iframe.attr("status", "closed");
         });
 
