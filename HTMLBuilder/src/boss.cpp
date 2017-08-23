bbf2a8f15aeaa7118dfea53db27e67cc97ab05be
Jekfer Bichon <jekfer.bichon@3ds.com>
2017-08-23 19:59:39 +0200

    some nice interface modifications

--- HTMLBuilder/src/boss.cpp
+++ HTMLBuilder/src/boss.cpp
@@ -2,6 +2,7 @@
 
 #include <QDebug>
 #include <QTextStream>
+#include <QDate>
 
 Boss::Boss(const QString& name,QObject *parent) : QObject(parent)
 {
@@ -18,11 +19,12 @@ void Boss::generateHTMLs(QTextStream& streamIndex){
 
     res << "<!DOCTYPE html>";
     res <<"<html>";
+    res << "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script>";
     res << "<head>";
     res << "    <meta charset=\"latin\">";
     res << "    <title>[ODS] " + this->name + " Logs</title>";
     res << "    <meta name=\"author\" content=\"EliphasNUIT\">";
-    res << "    <link href=\"../styles/style.css\" rel=\"stylesheet\" type=\"text/css\">";
+    res << "    <link href=\"../styles/styleBoss.css\" rel=\"stylesheet\" type=\"text/css\">";
     res << "</head>";
     res << "<body>";
     res << "    <h1>" + this->name + " [ODS]</h1>";
@@ -50,17 +52,28 @@ void Boss::generateHTMLs(QTextStream& streamIndex){
     qInfo() << "        treated " +this->name;  ;
 }
 
+QString toDate(const QString& date) {
+    return QDate::fromString(date,"yyyy_MM_dd").toString("dd/MM/yyyy");
+}
+
 QStringList Boss::getTries() {
     QDir triesDir(this->ressourcePath);
     QStringList tries = triesDir.entryList(QDir::Files, QDir::Name | QDir::Reversed );
     QStringList res;
     QStringListIterator it(tries);
+    res << "            <script type=\"text/javascript\"> function display(frameID) { var iframe = $(frameID); iframe.attr(\"src\",iframe.data(\"src\")); iframe.attr(\"width\",\"1200\"); iframe.attr(\"height\",\"1200\");} </script>";
+    res << "            <script type=\"text/javascript\"> function closeDisplay(frameID) { var iframe = $(frameID); iframe.attr(\"src\",\"about:blank\"); iframe.attr(\"width\",\"1\"); iframe.attr(\"height\",\"1\");} </script>";
     while(it.hasNext()) {
         QString aux(it.next());
         QString date = aux.split("-").at(0);
-        date.insert(4,"/");
-        date.insert(7,"/");
-        res << "<li> <a href=" + this->ressourceDir + aux + "\">" + date +"</a> </li>";
+        date.insert(4,"_");
+        date.insert(7,"_");
+        const QString path = this->ressourceDir + aux;
+        const QString iFrameName = "#" + date;
+        const QString displayName = "display(\'"+iFrameName+"\')";
+        const QString closeName = "closeDisplay(\'"+iFrameName+"\')";
+        res << "            <li> <button onclick=\""+displayName+"\"> Afficher les logs du " + toDate(date) + "</button> <button onclick=\""+closeName+"\"> Fermer les logs du " + toDate(date) + "</button> </li>";
+        res << "            <iframe id=\"" + date + "\" data-src="+path+"\" width=\"0\" height=\"0\" src=\"about:blank\"> </iframe>";
     }
     return res;
 }
