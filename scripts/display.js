25401fffd38c79c314e60bcdbe4bce54318dbd2b
Jekfer Bichon <jekfer.bichon@3ds.com>
2017-08-28 19:32:55 +0200

    updated compo for Mélo

--- scripts/display.js
+++ scripts/display.js
@@ -37,3 +37,35 @@ function display(frameID) {
     }
 
 }
+
+function display2(frameID) {
+    var iframe = $(frameID);
+    var status = iframe.attr("frameborder");
+    var init = iframe.attr("width");
+    if (init === "0") {
+        // init the button
+        iframe.load(function() {
+            var status = iframe.attr("frameborder");
+            if (status === "1") {
+                iframe.attr("frameborder", "3");
+                var height = "2200";
+                iframe.attr("height", height);
+            } else {
+                iframe.attr("width", "1");
+                iframe.attr("height", "1");
+                iframe.attr("frameborder", "0");
+            }
+        });
+        iframe.attr("width", "1");
+    }
+    if (status === "0") {
+        iframe.attr("frameborder", "1");
+        iframe.attr("height", "10");
+        iframe.attr("width", "1200");
+        iframe.attr("src", iframe.data("src"));
+
+    } else {
+        iframe.attr("src", "about:blank");
+    }
+
+}
