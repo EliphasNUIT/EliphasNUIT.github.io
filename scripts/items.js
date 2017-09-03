8c73e865ec61501df6095392d92cc05e7879fa24
EliphasNUIT <bichon.jekfer@gmail.com>
2017-09-03 02:04:18 +0200

    started items id

---
+++ scripts/items.js
@@ -0,0 +1,47 @@
+var trinket = {
+    ring1: 79712,
+    ring2: 81024,
+    amulet: 81658,
+    earring1: 79745,
+    earring2: 79444,
+    back: 80369,
+};
+
+/*var armor = {
+    light: {
+        head,
+        shoulder,
+        coat,
+        gloves,
+        leggings,
+        boots,
+    },
+    medium: {
+        head,
+        shoulder,
+        coat,
+        gloves,
+        leggings,
+        boots,
+    },
+    heavy: {
+        head,
+        shoulder,
+        coat,
+        gloves,
+        leggings,
+        boots,
+    }
+};*/
+
+var runes = {
+
+};
+
+var sigils = {
+
+};
+
+var weapons = {
+
+};
