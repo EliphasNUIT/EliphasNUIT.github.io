da8ccae3513572f7e49873bf8270c76635e83671
Jekfer Bichon <jekfer.bichon@3ds.com>
2017-09-05 16:15:34 +0200

    added pets info + more items

--- scripts/items.js
+++ scripts/items.js
@@ -9,27 +9,27 @@ var trinket = {
 
 var armor = {
     Light: {
-        helm:80751,
-        shoulders:80855,
-        coat:80811,
-        gloves:80838,
+        helm: 80751,
+        shoulders: 80855,
+        coat: 80811,
+        gloves: 80838,
         leggings: 80755,
-        boots:80815,
+        boots: 80815,
     },
     Medium: {
-        helm:80753,
-        shoulders:80825,
-        coat:80798,
-        gloves:80779,
-        leggings:80812,
-        boots:80836,
+        helm: 80753,
+        shoulders: 80825,
+        coat: 80798,
+        gloves: 80779,
+        leggings: 80812,
+        boots: 80836,
     },
     Heavy: {
-        helm:80832,
-        shoulders:80814,
+        helm: 80832,
+        shoulders: 80814,
         coat: 80818,
-        gloves:80834,
-        leggings:80845,
+        gloves: 80834,
+        leggings: 80845,
         boots: 80808,
     }
 };
@@ -37,41 +37,56 @@ var armor = {
 var runes = {
     "Superior Rune of Leadership": 70600,
     "Superior Rune of the Scholar": 24836,
-    "Superior Rune of the Berserker":71425,
-    "Superior Rune of the Aristocracy":24845,
-    "Superior Rune of the Monk":24842,
-    "Superior Rune of Balthazar":24765
+    "Superior Rune of the Berserker": 71425,
+    "Superior Rune of the Aristocracy": 24845,
+    "Superior Rune of the Monk": 24842,
+    "Superior Rune of Balthazar": 24765,
+    "Superior Rune of the Nightmare": 24848,
+    "Superior Rune of the Trapper": 67339,
+    "Superior Rune of the Krait" : 24762
 };
 
 var sigils = {
-    undefined: -1,
-    "Superior Sigil of Concentration":72339,
-    "Superior Sigil of Force":24615,
-    "Superior Sigil of Malice":44950,
-    "Superior Sigil of Geomancy":24605,
-    "Superior Sigil of Air":24554,
-    "Superior Sigil of Transference":74326,
-    "Superior Sigil of Water":24551,
-    "Superior Sigil of Smoldering":24624,
-    "Superior Sigil of Bursting":44944
+    "Superior Sigil of Concentration": 72339,
+    "Superior Sigil of Force": 24615,
+    "Superior Sigil of Malice": 44950,
+    "Superior Sigil of Geomancy": 24605,
+    "Superior Sigil of Air": 24554,
+    "Superior Sigil of Transference": 74326,
+    "Superior Sigil of Water": 24551,
+    "Superior Sigil of Smoldering": 24624,
+    "Superior Sigil of Bursting": 44944,
+    "Superior Sigil of Earth": 24560
 };
 
 var weapons = {
-    undefined: -1,
-    "Axe":81321,
-    "Dagger":81451,
-    "Mace":81514,
-    "Pistol":81270,
-    "Scepter":81400,
-    "Sword":81376,
-    "Focus":81503,
-    "Shield":81339,
-    "Torch":81327,
-    "Warhorn":81337,
-    "Greatsword":81309,
-    "Hammer":81374,
-    "Longbow":81289,
-    "Rifle":81528,
-    "Shortbow":81329,
-    "Staff":81382
+    "Axe": 81321,
+    "Dagger": 81451,
+    "Mace": 81514,
+    "Pistol": 81270,
+    "Scepter": 81400,
+    "Sword": 81376,
+    "Focus": 81503,
+    "Shield": 81339,
+    "Torch": 81327,
+    "Warhorn": 81337,
+    "Greatsword": 81309,
+    "Hammer": 81374,
+    "Longbow": 81289,
+    "Rifle": 81528,
+    "Shortbow": 81329,
+    "Staff": 81382
+};
+
+var foods = {
+    "Rare Veggie Pizza": 12464,
+    "Toxic Focusing Crystal": 48917,
+    "Furious Tuning Crystal": 67524,
+    "Bowl of Sweet and Spicy Butternut Squash Soup": 41569,
+    "Superior Sharpening Stone": 9443,
+    "Delicious Rice Ball": 68634,
+    "Bountiful Maintenance Oil": 67528,
+    "Fried Golden Dumpling": 68633,
+    "Bowl of Nopalitos Sauté": 66530,
+    "Magnanimous Maintenance Oil": 81157
 };
