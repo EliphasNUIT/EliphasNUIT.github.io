e840c6b47843b0eb03c0881e398cfb9b8fbed593
Jekfer Bichon <jekfer.bichon@3ds.com>
2017-10-10 18:26:49 +0200

    some factorisations

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,25 +1,12 @@
-function openChar1(name, evt) {
-    var teamTab = document.getElementsByClassName("t1");
+function openBuild(name, buttonPack, buildPack,  evt) {
+    var teamTab = document.getElementsByClassName(buildPack);
     for (var i = 0; i < teamTab.length; i++) {
         teamTab[i].style.display = "none"; 
     }
-    tablinks = document.getElementsByClassName("tablinks1");
+    tablinks = document.getElementsByClassName(buttonPack);
     for (var i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
     }
     document.getElementById(name).style.display = "block"; 
     evt.currentTarget.className += " w3-red";
 };
-
-function openChar2(name, evt) {
-    var teamTab = document.getElementsByClassName("t2");
-    for (var i = 0; i < teamTab.length; i++) {
-        teamTab[i].style.display = "none"; 
-    }
-    tablinks = document.getElementsByClassName("tablinks2");
-    for (var i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-    }
-    document.getElementById(name).style.display = "block"; 
-    evt.currentTarget.className += " w3-red";
-}
