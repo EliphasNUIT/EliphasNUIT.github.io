45e03bb26df7f5a6bc75491233b563025a7d93bc
Jekfer Bichon <jekfer.bichon@3ds.com>
2017-10-30 16:32:25 +0100

    migrated engineer

--- scripts/professionBuilds/professions/engineer.js
+++ scripts/professionBuilds/professions/engineer.js
@@ -1,167 +1,55 @@
-define(["professionBuilds/dataBuilds", "professionBuilds/utilities/classes"], function(buildData, classes) {
+define(["professionBuilds/dataBuilds", "professionBuilds/professionUtilities/professions"], function(
+    buildData,
+    professions
+) {
     "use strict";
-    var builds = buildData.builds;
-    var build = buildData.build;
-    var Engineer = classes.Engineer;
+    var Build = buildData.build;
+    var Profession = professions.Engineer;
+    var build = null;
     // Condi
     {
-        builds.set(
-            "engiCondi",
-            new build({
-                class: Engineer,
-                specializations: [
-                    {
-                        name: "Explosives",
-                        traits: ["Glass Cannon", "Aim-Assisted Rocket", "Shrapnel"]
-                    },
-                    {
-                        name: "Firearms",
-                        traits: ["Chemical Rounds", "Pinpoint Distribution", "Incendiary Powder"]
-                    },
-                    {
-                        name: "Tools",
-                        traits: ["Static Discharge", "Streamlined Kits", "Kinetic Battery"]
-                    }
-                ],
-                skills: ["Healing Turret", "Bomb Kit", "Grenade Kit", "Flamethrower", "Elite Mortar Kit"],
-                pets: null,
-                armor: [
-                    { slot: "helm", stat: "Viper's", rune: "Superior Rune of the Berserker" },
-                    {
-                        slot: "shoulders",
-                        stat: "Viper's",
-                        rune: "Superior Rune of the Berserker"
-                    },
-                    { slot: "coat", stat: "Viper's", rune: "Superior Rune of the Berserker" },
-                    {
-                        slot: "gloves",
-                        stat: "Viper's",
-                        rune: "Superior Rune of the Berserker"
-                    },
-                    {
-                        slot: "leggings",
-                        stat: "Viper's",
-                        rune: "Superior Rune of the Berserker"
-                    },
-                    { slot: "boots", stat: "Viper's", rune: "Superior Rune of the Berserker" }
-                ],
-                trinket: [
-                    { slot: "amulet", stat: "Viper's" },
-                    { slot: "ring1", stat: "Viper's" },
-                    { slot: "ring2", stat: "Viper's" },
-                    { slot: "back", stat: "Viper's" },
-                    { slot: "earring1", stat: "Viper's" },
-                    { slot: "earring2", stat: "Viper's" }
-                ],
-                weapons: [
-                    {
-                        slot: "mh1",
-                        type: "Pistol",
-                        stat: "Viper's",
-                        sigils: ["Superior Sigil of Geomancy"]
-                    },
-                    {
-                        slot: "oh1",
-                        type: "Pistol",
-                        stat: "Viper's",
-                        sigils: ["Superior Sigil of Malice"]
-                    },
-                    {
-                        slot: "mh2",
-                        type: "",
-                        stat: "",
-                        sigils: [""]
-                    },
-                    {
-                        slot: "oh2",
-                        type: "",
-                        stat: "",
-                        sigils: [""]
-                    }
-                ],
-                food: ["Rare Veggie Pizza", "Furious Tuning Crystal"]
-            })
-        );
+        build = new Build("engiCondi", Profession);
+        build.armor.setSingleStat("Viper's");
+        build.armor.setSingleRune("Superior Rune of the Berserker");
+        build.consumable.setConsumable("Rare Veggie Pizza", "Furious Tuning Crystal");
+        build.trinket.setSingleStat("Viper's");
+        build.wep1.setMainHand("Pistol", "Viper's", "Superior Sigil of Geomancy");
+        build.wep1.setOffHand("Pistol", "Viper's", "Superior Sigil of Malice");
+        build.wep2 = null;
+        build.specialization.setSpec("spec1", "Explosives", ["Glass Cannon", "Aim-Assisted Rocket", "Shrapnel"]);
+        build.specialization.setSpec("spec2", "Firearms", [
+            "Chemical Rounds",
+            "Pinpoint Distribution",
+            "Incendiary Powder"
+        ]);
+        build.specialization.setSpec("spec3", "Tools", ["Static Discharge", "Streamlined Kits", "Kinetic Battery"]);
+        build.skills.setHealSkill("Healing Turret");
+        build.skills.setUtilitySkills(["Bomb Kit", "Grenade Kit", "Flamethrower"]);
+        build.skills.setEliteSkill("Elite Mortar Kit");
     }
 
     // Power
     {
-        builds.set(
-            "holoPower",
-            new build({
-                class: Engineer,
-                specializations: [
-                    {
-                        name: "Explosives",
-                        traits: ["Glass Cannon", "Big Boomer", "Shrapnel"]
-                    },
-                    {
-                        name: "Firearms",
-                        traits: ["High Caliber", "Pinpoint Distribution", "Modified Ammunition"]
-                    },
-                    {
-                        name: "Holosmith",
-                        traits: ["Solar Focusing Lens", "Crystal Configuration: Eclipse", "Photonic Blasting Module"]
-                    }
-                ],
-                skills: ["Healing Turret", "Bomb Kit", "Grenade Kit", "Laser Disk", "Prime Light Beam"],
-                pets: null,
-                armor: [
-                    { slot: "helm", stat: "Berserker's", rune: "Superior Rune of the Scholar" },
-                    {
-                        slot: "shoulders",
-                        stat: "Berserker's",
-                        rune: "Superior Rune of the Scholar"
-                    },
-                    { slot: "coat", stat: "Berserker's", rune: "Superior Rune of the Scholar" },
-                    {
-                        slot: "gloves",
-                        stat: "Berserker's",
-                        rune: "Superior Rune of the Scholar"
-                    },
-                    {
-                        slot: "leggings",
-                        stat: "Berserker's",
-                        rune: "Superior Rune of the Scholar"
-                    },
-                    { slot: "boots", stat: "Berserker's", rune: "Superior Rune of the Scholar" }
-                ],
-                trinket: [
-                    { slot: "amulet", stat: "Berserker's" },
-                    { slot: "ring1", stat: "Berserker's" },
-                    { slot: "ring2", stat: "Berserker's" },
-                    { slot: "back", stat: "Berserker's" },
-                    { slot: "earring1", stat: "Berserker's" },
-                    { slot: "earring2", stat: "Berserker's" }
-                ],
-                weapons: [
-                    {
-                        slot: "mh1",
-                        type: "Rifle",
-                        stat: "Berserker's",
-                        sigils: ["Superior Sigil of Force", "Superior Sigil of Air"]
-                    },
-                    {
-                        slot: "oh1",
-                        type: "",
-                        stat: "",
-                        sigils: [""]
-                    },
-                    {
-                        slot: "mh2",
-                        type: "",
-                        stat: "",
-                        sigils: [""]
-                    },
-                    {
-                        slot: "oh2",
-                        type: "",
-                        stat: "",
-                        sigils: [""]
-                    }
-                ],
-                food: ["Bowl of Sweet and Spicy Butternut Squash Soup", "Superior Sharpening Stone"]
-            })
-        );
+        build = new Build("holoPower", Profession);
+        build.armor.setSingleStat("Berserker's");
+        build.armor.setSingleRune("Superior Rune of the Scholar");
+        build.consumable.setConsumable("Bowl of Sweet and Spicy Butternut Squash Soup", "Superior Sharpening Stone");
+        build.trinket.setSingleStat("Berserker's");
+        build.wep1.setTwoHand("Rifle", "Berserker's", "Superior Sigil of Force", "Superior Sigil of Air");
+        build.wep2 = null;
+        build.specialization.setSpec("spec1", "Explosives", ["Glass Cannon", "Big Boomer", "Shrapnel"]);
+        build.specialization.setSpec("spec2", "Firearms", [
+            "High Caliber",
+            "Pinpoint Distribution",
+            "Modified Ammunition"
+        ]);
+        build.specialization.setSpec("spec3", "Holosmith", [
+            "Solar Focusing Lens",
+            "Crystal Configuration: Eclipse",
+            "Photonic Blasting Module"
+        ]);
+        build.skills.setHealSkill("Healing Turret");
+        build.skills.setUtilitySkills(["Bomb Kit", "Grenade Kit", "Laser Disk"]);
+        build.skills.setEliteSkill("Prime Light Beam");
     }
 });
