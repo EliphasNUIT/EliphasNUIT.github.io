d9e0efe05eb3aca52ac0b7b886a9738e406d488b
Jekfer Bichon <jekfer.bichon@3ds.com>
2017-11-15 17:08:03 +0100

    updated druid builds

--- scripts/professionBuilds/professions/ranger.js
+++ scripts/professionBuilds/professions/ranger.js
@@ -62,12 +62,12 @@ define(["professionBuilds/dataBuilds", "professionBuilds/professionUtilities/pro
             this.armor.setSingleStat("Harrier's");
             this.armor.setSingleRune("Superior Rune of the Monk");
             this.consumable.setConsumable("Delicious Rice Ball", "Bountiful Maintenance Oil");
-            this.trinket.setStats(["Magi's","Minstrel's","Magi's","Minstrel's","Minstrel's","Minstrel's"]);
-            this.wep1.setTwoHand("Staff", "Magi's", "Superior Sigil of Transference", "Superior Sigil of Water");
-            this.wep2.setMainHand("Axe", "Magi's", "Superior Sigil of Transference");
-            this.wep2.setOffHand("Warhorn", "Magi's", "Superior Sigil of Concentration");
+            this.trinket.setSingleStat("Harrier's");
+            this.wep1.setTwoHand("Staff", "Harrier's", "Superior Sigil of Transference", "Superior Sigil of Water");
+            this.wep2.setMainHand("Axe", "Harrier's", "Superior Sigil of Transference");
+            this.wep2.setOffHand("Warhorn", "Harrier's", "Superior Sigil of Water");
             this.specialization.setSpec("spec1", "Skirmishing", ["Sharpened Edges", "Spotter", "Quick Draw"]);
-            this.specialization.setSpec("spec2", "Nature Magic", ["Allies' Aid", "Windborne Notes", "Nature's Vengeance"]);
+            this.specialization.setSpec("spec2", "Nature Magic", ["Instinctive Reaction", "Windborne Notes", "Nature's Vengeance"]);
             this.specialization.setSpec("spec3", "Druid", ["Primal Echoes", "Verdant Etching", "Grace of the Land"]);
             this.skills.pets =  ["Juvenile Rock Gazelle", "Juvenile Electric Wyvern"];
             this.skills.setHealSkill("Glyph of Rejuvenation");
