7f444cf69556dc20ef8f6a2185d847c738c0b16d
Jekfer Bichon <jekfer.bichon@3ds.com>
2018-01-08 16:55:34 +0100

    added berserker and spellbreaker builds

--- GW2Logs-dev/src/app/helpers/profBuildHelpers/professions/warrior.ts
+++ GW2Logs-dev/src/app/helpers/profBuildHelpers/professions/warrior.ts
@@ -27,6 +27,27 @@ class BerserkerBannerBuild extends WarriorBuild {
     }
 }
 
+class SpellbreakerBannerBuild extends WarriorBuild {
+    constructor(id, name) {
+        super(id, 'Spellbreaker - Banner - ' + name);
+        this.icon += 'spellbreaker.png';
+        this.armor.setStats(['Assassin\'s', 'Berserker\'s', 'Berserker\'s', 'Berserker\'s', 'Assassin\'s', 'Berserker\'s']);
+        this.armor.setSingleRune('Superior Rune of the Scholar');
+        this.consumable.setConsumable('Bowl of Sweet and Spicy Butternut Squash Soup', 'Superior Sharpening Stone');
+        this.trinket.setSingleStat('Berserker\'s');
+        this.wep1.setMainHand('Dagger', 'Berserker\'s', 'Superior Sigil of Force');
+        this.wep1.setOffHand('Dagger', 'Berserker\'s', 'Superior Sigil of Air');
+        this.wep2.setMainHand('Axe', 'Berserker\'s', 'Superior Sigil of Force');
+        this.wep2.setOffHand('Axe', 'Berserker\'s', 'Superior Sigil of Air');
+        this.specialization.setSpec('spec1', 'Strength', ['Peak Performance', 'Body Blow', 'Berserker\'s Power']);
+        this.specialization.setSpec('spec2', 'Tactics', ['Empowered', 'Empower Allies', 'Powerful Synergy']);
+        this.specialization.setSpec('spec3', 'Spellbreaker', ['Pure Strike', 'Loss Aversion', 'Revenge Counter']);
+        this.skills.setHealSkill('"To the Limit!"');
+        this.skills.setUtilitySkills(['Kick', 'Banner of Discipline', 'Banner of Strength']);
+        this.skills.setEliteSkill('Rampage');
+    }
+}
+
 export function warBuildMaker() {
     let build: ProfBuild = null;
     // Condi
@@ -35,6 +56,19 @@ export function warBuildMaker() {
 
         build = new BerserkerBannerBuild('bersCC', 'CC');
         build.skills.setUtilitySkills(['Wild Blow', 'Banner of Discipline', 'Banner of Strength']);
+
+        build = new BerserkerBannerBuild('bersFury', 'Fury');
+        build.skills.setUtilitySkills(['"For Great Justice!"', 'Banner of Discipline', 'Banner of Strength']);
+
+        build = new BerserkerBannerBuild('bersSplit', 'Split');
+        build.skills.setUtilitySkills(['Shattering Blow', 'Outrage', '']);
+    }
+    // power
+    {
+        build = new SpellbreakerBannerBuild('spellbrk', 'Standart');
+
+        build = new SpellbreakerBannerBuild('spellbrkFury', 'Fury');
+        build.skills.setUtilitySkills(['"For Great Justice!"', 'Banner of Discipline', 'Banner of Strength']);
     }
 }
 
