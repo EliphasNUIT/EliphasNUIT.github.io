7cff517909cb1a3027483b4d4604dfc4ed03f627
EliphasNUIT <bichon.jekfer@gmail.com>
2018-03-17 22:54:40 +0100

    moving documentation

--- GW2Logs-dev/documentation/js/routes/routes_index.js
+++
@@ -1,1 +0,0 @@
-var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[{"name":"routes","filename":"src/app/app-routing.module.ts","module":"AppRoutingModule","children":[{"path":"","redirectTo":"/main","pathMatch":"full"},{"path":"db","component":"DbBuildComponent"},{"path":"main","component":"BossesComponent"}],"kind":"module"}]}
